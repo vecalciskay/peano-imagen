ea28b14d6d01c1aec6f170fcdbe0e02f693bff9c
Vladimir <vladimir.calderon@gmail.com>
2023-08-30 10:35:14 -0400

    Carga inicial, se completara luego del 8 de septiembre

---
+++ src/main/java/peano/PeanoImagen.java
@@ -0,0 +1,12 @@
+package peano;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class PeanoImagen {
+    private static final Logger logger = LogManager.getRootLogger();
+
+    public static void main(String[] args) {
+        logger.info("PeanoImagen");
+    }
+}
